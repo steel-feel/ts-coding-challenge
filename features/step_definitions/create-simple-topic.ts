fea8377ed3cd1f980caf8f02d23402638ad3125e
Himank Jain <himankjain@yahoo.in>
2025-12-05 14:41:54 +0530

    feature 1 complete

--- features/step_definitions/create-simple-topic.ts
+++ features/step_definitions/create-simple-topic.ts
@@ -3,6 +3,7 @@ import {
   AccountBalanceQuery,
   AccountId,
   Client,
+  KeyList,
   PrivateKey, RequestType,
   TopicCreateTransaction, TopicInfoQuery,
   TopicMessageQuery, TopicMessageSubmitTransaction
@@ -24,25 +25,36 @@ Given(/^a first account with more than (\d+) hbars$/, async function (expectedBa
   this.privKey = privKey
   client.setOperator(this.account, privKey);
 
+  // console.log(`evm address : ${account.toEvmAddress()} , accountId : ${acc.id}`);
+  
   //Create the query request
   const query = new AccountBalanceQuery().setAccountId(account);
   const balance = await query.execute(client)
+
+  // console.log(`balance : ${balance.hbars.toBigNumber().toNumber()}`);
   assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance)
 });
 
 When(/^A topic is created with the memo "([^"]*)" with the first account as the submit key$/, async function (memo: string) {
-  const tx = await new TopicCreateTransaction({ topicMemo: memo }).execute(client);
+  const tx = await new TopicCreateTransaction({ topicMemo: memo, submitKey: this.privKey.publicKey })
+  .execute(client);
   const receipt = await tx.getReceipt(client);
   this.topicId = receipt.topicId;
+  assert.ok(this.topicId)
 });
 
-When(/^The message "([^"]*)" is published to the topic$/, async function (message: string) {
-  await new TopicMessageSubmitTransaction({ topicId: this.topicId, message }).execute(client)
-  //  const receipt = await tx.getReceipt(client);
+When(/^The message "([^"]*)" is published to the topic$/, async function (message: string) {  
+ const tx = await new TopicMessageSubmitTransaction({ topicId: this.topicId, message }).execute(client)
+ const receipt = await tx.getReceipt(client);
+
+  console.log(`receipt.status ${receipt.status}`);
+  
+
+ assert.ok(receipt)
 });
 
 Then(/^The message "([^"]*)" is received by the topic and can be printed to the console$/, async function (message: string) {
-  const q = await new TopicMessageQuery({ topicId: this.topicId })
+  await new TopicMessageQuery({ topicId: this.topicId })
     .subscribe(
       client,
       (error) => console.log(`Error: ${error}`),
@@ -50,14 +62,39 @@ Then(/^The message "([^"]*)" is received by the topic and can be printed to the 
     );
 });
 
-Given(/^A second account with more than (\d+) hbars$/, async function () {
+Given(/^A second account with more than (\d+) hbars$/, async function (expectedBalance: number) {
+  const acc = accounts[1]
+  const account: AccountId = AccountId.fromString(acc.id);
+  this.account2 = account
+  const privKey: PrivateKey = PrivateKey.fromStringED25519(acc.privateKey);
+  this.privKey2 = privKey
 
+  //Create the query request
+  const query = new AccountBalanceQuery().setAccountId(account);
+  const balance = await query.execute(client)
+  assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance)
 });
 
-Given(/^A (\d+) of (\d+) threshold key with the first and second account$/, async function () {
-
+Given(/^A (\d+) of (\d+) threshold key with the first and second account$/, async function (threshold: number, totalKeys : number) {
+  const publicKeyList = [this.privKey.publicKey, this.privKey2.publicKey];
+  const thresholdKey =  new KeyList(publicKeyList,threshold); 
+  this.thresholdKey = thresholdKey
+  assert.ok(this.thresholdKey.toArray().length == totalKeys)
+  assert.ok(this.thresholdKey.threshold == threshold)
 });
 
-When(/^A topic is created with the memo "([^"]*)" with the threshold key as the submit key$/, async function () {
+When(/^A topic is created with the memo "([^"]*)" with the threshold key as the submit key$/, async function (memo: string) {
+    const tx = await new TopicCreateTransaction({ topicMemo: memo, submitKey: this.thresholdKey })
+    const freezedTx = await tx.freezeWith(client);
 
+    const signature = this.privKey.signTransaction(freezedTx);
+    const signedTransaction = freezedTx.addSignature(this.privKey.publicKey, signature)
+
+    const submitTx = await signedTransaction.execute(client)
+    const receipt = await submitTx.getReceipt(client);
+
+    this.topicId = receipt.topicId
+    assert.ok(this.topicId)
+    
 });
+
